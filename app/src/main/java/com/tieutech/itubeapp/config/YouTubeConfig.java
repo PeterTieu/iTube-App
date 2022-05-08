2345d20401c1dfbe08803bccaa6a4507d2791975
petertieu <tieu.peter.v@gmail.com>
2022-05-08 20:58:49 +1000

    Commit

--- app/src/main/java/com/tieutech/itubeapp/config/YouTubeConfig.java
+++ app/src/main/java/com/tieutech/itubeapp/config/YouTubeConfig.java
@@ -1,11 +1,13 @@
 package com.tieutech.itubeapp.config;
 
+//ABOUT: Class that defines the API key from Google Cloud Platform
 public class YouTubeConfig {
-
+    //Variable
     private  static final  String API_KEY = "AIzaSyB7Gyr0NuskJuYOtlgKNGyYjEwk1GJiImQ";
 
-
+    //Constructor
     public YouTubeConfig() { }
 
+    //Getter
     public static String getApiKey() { return API_KEY; }
 }
